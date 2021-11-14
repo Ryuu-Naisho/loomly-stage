0cda4df74beb238a93ee5d0fa24eb448a4de7ca0
Ryuu365 <no@email.please>
2021-11-14 18:41:10 +0000

    Create react project.

--- installs.sh
+++ installs.sh
@@ -2,3 +2,9 @@
 
 
 sudo apt install npm
+npm install --save react-router-dom
+npm i -g npx
+npm install -g n
+#Using node version v16.13.0
+n stable
+npx create-react-app loomly-stage --template cra-template-pwa-typescript
